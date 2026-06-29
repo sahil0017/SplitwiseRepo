4ba1f30dcb009fb8dca0bcbc1f64d5a1ebc54c27
sahilkanungo <kanungosahil123@gmail.com>
2026-06-29 20:19:56 +0530

    updated config

--- src/main/java/com/system_design/splitwise/Configuration/SwaggerConfig.java
+++ src/main/java/com/system_design/splitwise/Configuration/SwaggerConfig.java
@@ -3,7 +3,6 @@ package com.system_design.splitwise.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +12,7 @@ import java.util.List;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI splitwiseOpenAPI() {
-
-        Server localServer = new Server();
-        localServer.setUrl("http://localhost:8080");
-        localServer.setDescription("Local Server");
+    public OpenAPI customOpenAPI() {
 
         Contact contact = new Contact();
         contact.setName("SplitWise System");
@@ -30,7 +25,6 @@ public class SwaggerConfig {
                 .contact(contact);
 
         return new OpenAPI()
-                .info(info)
-                .servers(List.of(localServer));
+                .info(info);
     }
 }
